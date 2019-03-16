d2dcfb1dac4e64c2959c1bd89c2fbf1a334706bb
DevOfLua <45329766+DevOfLua@users.noreply.github.com>
2019-03-16 09:06:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,34 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-
+var express = require('express')
+var app = express();
 const Statsjs = require("./Modules/Stats.js");
 
-console.log("Starting")
+var port = process.env.PORT || 9000;
 
-const port = process.argv[2] || 9000;
+app.use(express.static(__dirname + '/Website'));
 
-const mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.wav': 'audio/wav',
-  '.mp3': 'audio/mpeg',
-  '.svg': 'image/svg+xml',
-  '.pdf': 'application/pdf',
-  '.doc': 'application/msword',
-  '.eot': 'appliaction/vnd.ms-fontobject',
-  '.ttf': 'aplication/font-sfnt'
-};
-
-http.createServer(function (req, res) {
-  console.log(req)
-  if (req.method == "POST") {
-    //95206881
-    if (!JSON.stringify(req.rawHeaders).includes(2954706683)) {
-      return;
-    }
-
-    var body = '';
-
-    req.on('data', function (data) {
-        body += data;
-    });
+app.get('/', function(req, res) {
+    if (req.method == "POST") {
+        //95206881
+        if (!JSON.stringify(req.rawHeaders).includes(2954706683)) {
+          return;
+        }
     
-    req.on('end', function () {
-      if (req.url == "/Website/API/Data.json") {
-        Statsjs.run(body);
-        return;
-      }
-    });
-  }
+        var body = '';
+    
+        req.on('data', function (data) {
+            body += data;
+        });
+        
+        req.on('end', function () {
+          if (req.url == "/Website/API/Data.json") {
+            Statsjs.run(body);
+            return;
+          }
+        });
+    }    
+	res.render('index');
+})
 
-  const parsedUrl = url.parse(req.url);
-  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
-  let pathname = path.join(__dirname, sanitizePath);
-
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
-    }
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '/index.html';
-    }
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        const ext = path.parse(pathname).ext;
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-
-
-}).listen(parseInt(port));
-
-console.log(`Server listening on port ${port}`);
+app.listen(port, function() {
+	console.log('app running')
+})
